ee1c636d548b2eab2bcb71c9d29ca0c8fb394bdb
sinagun <38187655+sinagun@users.noreply.github.com>
2021-08-16 07:38:45 +0430

    0.5

--- main.py
+++ main.py
@@ -75,6 +75,7 @@ def button_divide():
 
 # Define Buttons
 
+# Define Buttons number
 button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1)).grid(row=3, column=0)
 button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2)).grid(row=3, column=1)
 button_3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3)).grid(row=3, column=2)
@@ -85,14 +86,16 @@ button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click
 button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8)).grid(row=1, column=1)
 button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9)).grid(row=1, column=2)
 button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0)).grid(row=4, column=0)
+# Define Buttons number END
+
+# Define Buttons operator
 button_add = Button(root, text="+", padx=39, pady=20, command=button_add).grid(row=5, column=0)
 button_equal = Button(root, text="=", padx=91, pady=20, command=button_equal).grid(row=5, column=1, columnspan=2)
 button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_clear).grid(row=4, column=1, columnspan=2)
-
 button_subtract = Button(root, text="-", padx=41, pady=20, command=button_subtract).grid(row=6, column=0)
 button_multiply = Button(root, text="*", padx=40, pady=20, command=button_multiply).grid(row=6, column=1)
 button_divide = Button(root, text="/", padx=41, pady=20, command=button_divide).grid(row=6, column=2)
-
+# Define Buttons operator
 
 
 root.mainloop()
